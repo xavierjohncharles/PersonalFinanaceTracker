f9249d738a088f4a445aa1a330cacd43045febf3
Xavier John Charles <crazyxydj@gmail.com>
2024-08-29 19:53:08 +0100

    Initial Commit

---
+++ PersonalFinanaceTracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PersonalFinanaceTracker
+//
+//  Created by Xavier John-Charles on 29/08/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PersonalFinanaceTracker/PersonalFinanaceTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalFinanaceTrackerApp.swift
+//  PersonalFinanaceTracker
+//
+//  Created by Xavier John-Charles on 29/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct PersonalFinanaceTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
